49e513312bb817e1843f62693bccbec83cd00d21
shannaurelle <shannaurelleg@gmail.com>
2023-03-23 17:37:41 +0800

    chore: FCFS initial commit

---
+++ Algorithm/FCFS.java
@@ -0,0 +1,17 @@
+package Algorithm;
+import java.util.ArrayList;
+
+public class FCFS{
+    private int[] bursts;
+    private int[] arrivals;
+    private int[] waitings;
+    private int[] turnarounds;
+    private int[] ave_turnarounds;
+    private int[] ave_waitings;
+
+    public FCFS(){
+
+    }
+
+
+}
