ec42a66627bbc7096446a88b2eb5282f37aa2977
shannaurelle <shannaurelleg@gmail.com>
2023-03-31 01:01:19 +0800

    temp sjf

--- Algorithm/SJF.java
+++ Algorithm/SJF.java
@@ -1,24 +1,31 @@
 package Algorithm;
 
+import java.util.stream.IntStream;
+
 public class SJF {
     private int[] arrivalTime;
     private int[] burstTime;
     private int numProcesses;
-    private int[] turnaroundTime;
-    private int[] waitingTime;
-    private int[] remainingTime;
-    private int[] completionTime;
+    private int[] turnaroundTimes;
+    private int[] waitingTimes;
+    private int[] remainingTimes;
+    private int[] completionTimes;
+    private int[] startTimes;
+    private double averageTurnaroundTime;
+    private double averageWaitingTime;
+
     
     public SJF(int[] arrivalTime, int[] burstTime, int numProcesses) {
         this.arrivalTime = arrivalTime;
         this.burstTime = burstTime;
         this.numProcesses = numProcesses;
-        this.turnaroundTime = new int[numProcesses];
-        this.waitingTime = new int[numProcesses];
-        this.remainingTime = new int[numProcesses];
-        this.completionTime = new int[numProcesses];
+        this.turnaroundTimes = new int[numProcesses];
+        this.waitingTimes = new int[numProcesses];
+        this.remainingTimes = new int[numProcesses];
+        this.completionTimes = new int[numProcesses];
+        this.startTimes = new int[numProcesses];
         for (int i = 0; i < numProcesses; i++) {
-            this.remainingTime[i] = burstTime[i];
+            this.remainingTimes[i] = burstTime[i];
         }
         this.calculate();
     }
@@ -31,12 +38,28 @@ public class SJF {
         return burstTime;
     }
     
-    public int[] getTurnaroundTime() {
-        return turnaroundTime;
+    public int[] getTurnaroundTimes() {
+        return turnaroundTimes;
     }
     
-    public int[] getWaitingTime() {
-        return waitingTime;
+    public int[] getWaitingTimes() {
+        return waitingTimes;
+    }
+
+    public int[] getCompletionTimes() {
+        return completionTimes;
+    }
+
+    public int[] getStartTimes() {
+        return startTimes;
+    }
+
+    public double getAverageTurnaroundTime() {
+        return averageTurnaroundTime;
+    }
+
+    public double getAverageWaitingTime() {
+        return averageWaitingTime;
     }
     
     public String getGanttChart() {
@@ -48,8 +71,8 @@ public class SJF {
             int minRemainingTime = Integer.MAX_VALUE;
             int nextProcess = -1;
             for (int i = 0; i < numProcesses; i++) {
-                if (arrivalTime[i] <= currentTime && completed[i] == 0 && remainingTime[i] < minRemainingTime) {
-                    minRemainingTime = remainingTime[i];
+                if (arrivalTime[i] <= currentTime && completed[i] == 0 && remainingTimes[i] < minRemainingTime) {
+                    minRemainingTime = remainingTimes[i];
                     nextProcess = i;
                 }
             }
@@ -58,9 +81,9 @@ public class SJF {
                 currentTime++;
             } else {
                 sb.append("P" + nextProcess);
-                currentTime += remainingTime[nextProcess];
-                remainingTime[nextProcess] = 0;
-                completionTime[nextProcess] = currentTime;
+                currentTime += remainingTimes[nextProcess];
+                remainingTimes[nextProcess] = 0;
+                completionTimes[nextProcess] = currentTime;
                 completed[nextProcess] = 1;
                 completedCount++;
             }
@@ -71,20 +94,25 @@ public class SJF {
     public void calculate() {
         int[] turnaroundTime = new int[numProcesses];
         int[] waitingTime = new int[numProcesses];
+        int[] startTime = new int[numProcesses];
         for (int i = 0; i < numProcesses; i++) {
-            turnaroundTime[i] = completionTime[i] - arrivalTime[i];
+            turnaroundTime[i] = completionTimes[i] - arrivalTime[i];
             waitingTime[i] = turnaroundTime[i] - burstTime[i];
+            startTime[i] = waitingTime[i] + arrivalTime[i];
         }
-        this.turnaroundTime = turnaroundTime;
-        this.waitingTime = waitingTime;
+        this.turnaroundTimes = turnaroundTime;
+        this.waitingTimes = waitingTime;
+        this.startTimes = startTime;
+        averageWaitingTime = IntStream.range(0,waitingTimes.length).boxed()
+            .mapToInt(i -> waitingTimes[i]).average().orElse(0.0);
+        averageTurnaroundTime = IntStream.range(0,turnaroundTimes.length).boxed()
+            .mapToInt(i -> turnaroundTimes[i]).average().orElse(0.0); 
     }
 
     public static void main(String[] args){
         int[] bursts = {1,4,2};
         int[] arrivals = {1,3,3};
         SJF test_sjf = new SJF(bursts, arrivals, bursts.length);
-
-        test_sjf.calculate();
         System.out.println(test_sjf.getGanttChart());
     }
 }
