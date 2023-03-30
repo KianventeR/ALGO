9f10945ed7063f4db6a22e144745fb3893b5d350
shannaurelle <shannaurelleg@gmail.com>
2023-03-30 12:27:12 +0800

    working SJF algorithm

--- Algorithm/SJF.java
+++ Algorithm/SJF.java
@@ -1,72 +1,89 @@
 package Algorithm;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Comparator;
-import java.util.Arrays;
 
 public class SJF {
+    private int[] arrivalTime;
+    private int[] burstTime;
+    private int numProcesses;
+    private int[] turnaroundTime;
+    private int[] waitingTime;
+    private int[] remainingTime;
+    private int[] completionTime;
     
-    public static void main(String[] args) {
-        int[] arrivalTime = {0, 2, 4, 5, 7};
-        int[] burstTime = {5, 3, 1, 2, 6};
-        int n = arrivalTime.length;
-
-        // create a list of processes
-        List<Process> processes = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            processes.add(new Process(i+1, arrivalTime[i], burstTime[i]));
+    public SJF(int[] arrivalTime, int[] burstTime, int numProcesses) {
+        this.arrivalTime = arrivalTime;
+        this.burstTime = burstTime;
+        this.numProcesses = numProcesses;
+        this.turnaroundTime = new int[numProcesses];
+        this.waitingTime = new int[numProcesses];
+        this.remainingTime = new int[numProcesses];
+        this.completionTime = new int[numProcesses];
+        for (int i = 0; i < numProcesses; i++) {
+            this.remainingTime[i] = burstTime[i];
         }
-
-        // sort the list by burst time
-        processes.sort(Comparator.comparing(Process::getBurstTime));
-
-        // run the processes and calculate waiting time and turnaround time
-        int[] waitingTime = new int[n];
-        int[] turnaroundTime = new int[n];
+    }
+    
+    public int[] getArrivalTime() {
+        return arrivalTime;
+    }
+    
+    public int[] getBurstTime() {
+        return burstTime;
+    }
+    
+    public int[] getTurnaroundTime() {
+        return turnaroundTime;
+    }
+    
+    public int[] getWaitingTime() {
+        return waitingTime;
+    }
+    
+    public String getGanttChart() {
+        StringBuilder sb = new StringBuilder();
         int currentTime = 0;
-        for (int i = 0; i < n; i++) {
-            Process p = processes.get(i);
-            waitingTime[p.getId()-1] = currentTime - p.getArrivalTime();
-            turnaroundTime[p.getId()-1] = waitingTime[p.getId()-1] + p.getBurstTime();
-            currentTime += p.getBurstTime();
+        int[] completed = new int[numProcesses];
+        int completedCount = 0;
+        while (completedCount < numProcesses) {
+            int minRemainingTime = Integer.MAX_VALUE;
+            int nextProcess = -1;
+            for (int i = 0; i < numProcesses; i++) {
+                if (arrivalTime[i] <= currentTime && completed[i] == 0 && remainingTime[i] < minRemainingTime) {
+                    minRemainingTime = remainingTime[i];
+                    nextProcess = i;
+                }
+            }
+            if (nextProcess == -1) {
+                sb.append("-");
+                currentTime++;
+            } else {
+                sb.append("P" + nextProcess);
+                currentTime += remainingTime[nextProcess];
+                remainingTime[nextProcess] = 0;
+                completionTime[nextProcess] = currentTime;
+                completed[nextProcess] = 1;
+                completedCount++;
+            }
         }
-
-        // print the results
-        System.out.println("Process\tArrival Time\tBurst Time\tWaiting Time\tTurnaround Time");
-        for (int i = 0; i < n; i++) {
-            Process p = processes.get(i);
-            System.out.printf("%d\t%d\t\t%d\t\t%d\t\t%d\n", p.getId(), p.getArrivalTime(), p.getBurstTime(),
-                    waitingTime[p.getId()-1], turnaroundTime[p.getId()-1]);
+        return sb.toString();
+    }
+    
+    public void calculate() {
+        int[] turnaroundTime = new int[numProcesses];
+        int[] waitingTime = new int[numProcesses];
+        for (int i = 0; i < numProcesses; i++) {
+            turnaroundTime[i] = completionTime[i] - arrivalTime[i];
+            waitingTime[i] = turnaroundTime[i] - burstTime[i];
         }
-
-        // calculate and print the average waiting time and turnaround time
-        double avgWaitingTime = Arrays.stream(waitingTime).average().getAsDouble();
-        double avgTurnaroundTime = Arrays.stream(turnaroundTime).average().getAsDouble();
-        System.out.printf("Average Waiting Time: %.2f\n", avgWaitingTime);
-        System.out.printf("Average Turnaround Time: %.2f\n", avgTurnaroundTime);
+        this.turnaroundTime = turnaroundTime;
+        this.waitingTime = waitingTime;
     }
 
-    static class Process {
-        private final int id;
-        private final int arrivalTime;
-        private final int burstTime;
+    public static void main(String[] args){
+        int[] bursts = {1,4,2};
+        int[] arrivals = {1,3,3};
+        SJF test_sjf = new SJF(bursts, arrivals, bursts.length);
 
-        public Process(int id, int arrivalTime, int burstTime) {
-            this.id = id;
-            this.arrivalTime = arrivalTime;
-            this.burstTime = burstTime;
-        }
-
-        public int getId() {
-            return id;
-        }
-
-        public int getArrivalTime() {
-            return arrivalTime;
-        }
-
-        public int getBurstTime() {
-            return burstTime;
-        }
+        test_sjf.calculate();
+        System.out.println(test_sjf.getGanttChart());
     }
 }
