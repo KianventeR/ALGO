a5db297f63043cca87a2e23b579972743bb7103c
shannaurelle <shannaurelleg@gmail.com>
2023-03-31 22:46:28 +0800

    saved job and added generate to constructor

--- src/npps.java
+++ src/npps.java
@@ -68,6 +68,7 @@ public class npps {
     public npps(int[] burst, int[] arrival, int[] priority){
         this.jobs = new ArrayList<>();
         addJobs(arrival, burst, priority);
+        String res = generateGanttChart();
     }
 
     public void addJob(Job job) {
@@ -174,6 +175,7 @@ public class npps {
         StringBuilder chart = new StringBuilder();
         int currentTime = 0;
         ArrayList<Job> jobsCopy = new ArrayList<>(jobs);
+        ArrayList<Job> jobsSaved = new ArrayList<>();
 
         while (!jobsCopy.isEmpty()) {
             Job selectedJob = null;
@@ -192,12 +194,13 @@ public class npps {
                 currentTime += selectedJob.getBurstTime();
                 selectedJob.setFinishTime(currentTime);
                 chart.append(selectedJob.getName());
+                jobsSaved.add(selectedJob);
             } else {
                 chart.append("-");
                 currentTime++;
             }
         }
-
+        jobs = jobsSaved;
         return chart.toString();
     }
 
